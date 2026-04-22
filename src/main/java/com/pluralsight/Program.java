7c120a763fae726386e461d71eb3f5f2f8e8e6b5
Yumozh <juliiamozhchil@gmail.com>
2026-04-22 13:14:05 -0500

    feat: find exact time of action

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        LocalDateTime logTime = LocalDateTime.now();
+        
+
+        //Prompt actions
+        String programStart = noteTime(logTime);
+        System.out.println("Enter your search term (X to exit):");
+        String userAction = scan.nextLine();
+
+        //Note date and time of action
+
+
+
+        //Write to a file
+
+    }
+
+    private static String noteTime(LocalDateTime logTime) {
+        DateTimeFormatter exactTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return logTime.format(exactTime);
+    }
 }
